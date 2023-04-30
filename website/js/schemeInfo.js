6f1611e00bc4ed6c815bdc821c3da98bfb68881b
Prakhar Makhija <matcdac@gmail.com>
2023-04-30 11:38:23 +0530

    [meta] adding latest meta-data and formatting the json

--- website/js/schemeInfo.js
+++ website/js/schemeInfo.js
@@ -1,5 +1,5 @@
 
-let schemeInfoUrl = "https://raw.githubusercontent.com/NationalPensionScheme/NationalPensionScheme.GitHub.io/main/meta-data/pension-fund-schemes-v3.json";
+let schemeInfoUrl = "https://raw.githubusercontent.com/NationalPensionScheme/NationalPensionScheme.GitHub.io/main/meta-data/pension-fund-schemes-v5.json";
 
 let schemeInfo = [];
 let schemeIdVsSchemeName = {};
