adff796fa45a1f612dfe53ca61896d6eee501265
Prakhar Makhija <matcdac@gmail.com>
2026-04-16 16:54:18 +0530

    [nps 3.0] temporary commit

---
+++ microservice/src/main/java/open/source/nps/enumeration/ManagedCategorization.java
@@ -0,0 +1,11 @@
+package open.source.nps.enumeration;
+
+public enum ManagedCategorization {
+
+    DIRECT, // e-NPS platform or Central Recordkeeping Agency (CRA)
+    POP, // Point of Presence
+    GS, // Government Sector
+    
+    ;
+
+}
